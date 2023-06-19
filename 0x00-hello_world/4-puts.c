983148d8315c3e31a4a4f8e64bfabc46281944bf
rasaqsodiq1221 <rasaqsodiq1221@gmail.com>
2023-06-19 10:49:28 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
+#include  <stdio.h>
 /**
  * main - a program that prints a line with puts function
  * Return 0 (success)
  */
-#include <stdio.h>
 int main(void) 
 {
 puts("\"Programming is like building a multilingual puzzle");
